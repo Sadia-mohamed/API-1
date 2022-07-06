1c51579e0a9207c517ba4a900bad5fd283358168
sadia-mohamed <sacduushm@gmail.com>
2022-07-06 16:45:53 +0300

    initial commite

---
+++ app.js
@@ -0,0 +1,28 @@
+const cat_btn = document.getElementById("cat_btn");
+const fox_btn = document.getElementById("fox_btn");
+const cat_result = document.getElementById("cat_result");
+const fox_result = document.getElementById("fox_result");
+
+
+cat_btn.addEventListener('click', getRandomCat);
+fox_btn.addEventListener('click', getRandomFox);
+
+function getRandomCat() {
+    fetch("https://aws.random.cat/meow")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data.file);
+        cat_result.innerHTML = `<img src = ${data.file} alt='cat'>`;
+    });
+}
+
+
+
+
+function getRandomFox(){
+    fetch("https://randomfox.ca/floof/").then((response) => response.json()).then((data) =>{
+        console.log(data.image);
+        fox_result.innerHTML = `<img src = ${data.image} alt='fox'>`;
+    
+    });
+}
